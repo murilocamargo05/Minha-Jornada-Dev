39019df57a34f3716ff279fb976301df3dfb4c0d
Murilo Camargo <fonsecacamargomurilo@gmail.com>
2025-04-17 16:24:24 -0300

    Create LoopsSimples.java

---
+++ LoopsSimples.java
@@ -0,0 +1,17 @@
+public class LoopsSimples{
+    public static void main(String[] args){
+        int i;
+        for(i = 1; i<=10; i++) {
+            System.out.println(i); //Printa números sequenciais de 1 a 10.
+        }
+            System.out.println("");
+        int x = 2;
+        while(x<=20){
+            if(x%2 == 0){
+                System.out.println(x); //Printa números pares sequencaisa de 2 a 20.
+            }
+                x++;
+        }
+    }
+
+}
